d8102e150da96b4878fc9f563200c9f7ee5e8c11
kasia.kordys <kasia.kordys@gmail.com>
2023-09-13 14:46:19 +0300

    add planner item

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,23 @@ function App() {
       <form className="new-item-form">
         <div className="form-row">
           <label htmlFor="item">New item </label>
-          <input type="text" id="item"></input>
+          <input
+            type="text"
+            id="item" // onChange={handleChange}
+          ></input>
         </div>
         <button className="btn">Add</button>
       </form>
+      <h1 className="header">Planner</h1>
+      <ul className="list">
+        <li>
+          <label>
+            item
+            <input type="checkbox" />
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
     </>
   );
 }
